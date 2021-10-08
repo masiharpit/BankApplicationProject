48063f96563ccb6f5497ba514f8e047526c1b3ce
gitikapinjani1 <88777281+gitikapinjani1@users.noreply.github.com>
2021-10-08 15:30:39 +0530

    Add files via upload

--- BankingApplication/src/main/java/com/techment/entity/Account.java
+++ BankingApplication/src/main/java/com/techment/entity/Account.java
@@ -1,5 +1,58 @@
 package com.techment.entity;
 
-public class Account {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="account")
+public class Account {
+	
+	@Id
+	long accountId;
+	
+	@Column(name="interest_Rate")
+	double interestRate;
+	
+	@Column(name="balance")
+	double balance;
+	
+	@Column(name="date_Of_Opening")
+	LocalDate dateOfOpening;
+	
+	public long getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(long accountId) {
+		this.accountId = accountId;
+	}
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public LocalDate getDateOfOpening() {
+		return dateOfOpening;
+	}
+
+	public void setDateOfOpening(LocalDate dateOfOpening) {
+		this.dateOfOpening = dateOfOpening;
+	}
 }
