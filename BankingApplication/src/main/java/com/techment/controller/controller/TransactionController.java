3f26efe04fb4199cfa393d6b5348de379a163eda
gitikapinjani1 <88777281+gitikapinjani1@users.noreply.github.com>
2021-10-08 23:53:07 +0530

    Add files via upload

---
+++ BankingApplication/src/main/java/com/techment/controller/controller/TransactionController.java
@@ -0,0 +1,19 @@
+package com.techment.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.techment.service.ITransactionService;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value="/transaction")
+public class TransactionController {
+
+	@Autowired
+	private ITransactionService service;
+	
+	
+}
