24eecc8dfe726517e303aa8846317ff7f0954a40
gitikapinjani1 <88777281+gitikapinjani1@users.noreply.github.com>
2021-10-08 23:52:02 +0530

    Add files via upload

---
+++ BankingApplication/src/main/java/com/techment/entity/entity/Admin.java
@@ -0,0 +1,63 @@
+package com.techment.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Admin5")
+public class Admin {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	 Long adminId;
+	 String adminName;
+	 String adminContact;
+	 String adminEmailId;
+	
+	
+	
+	public Admin() {
+		super();
+	}
+	public Admin(Long adminId, String adminName, String adminContact, String adminEmailId) {
+		super();
+		this.adminId = adminId;
+		this.adminName = adminName;
+		this.adminContact = adminContact;
+		this.adminEmailId = adminEmailId;
+	}
+	public Long getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(Long adminId) {
+		this.adminId = adminId;
+	}
+	public String getAdminName() {
+		return adminName;
+	}
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+	public String getAdminContact() {
+		return adminContact;
+	}
+	public void setAdminContact(String adminContact) {
+		this.adminContact = adminContact;
+	}
+	public String getAdminEmailId() {
+		return adminEmailId;
+	}
+	public void setAdminEmailId(String adminEmailId) {
+		this.adminEmailId = adminEmailId;
+	}
+	public void setStatus(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
