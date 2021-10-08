d4cc3f2f3b27ebc0bb3534c36f8aa87cca0552db
gitikapinjani1 <88777281+gitikapinjani1@users.noreply.github.com>
2021-10-09 00:10:38 +0530

    Delete BankingApplication/src/main/java/com/techment/dao/dao directory

--- BankingApplication/src/main/java/com/techment/dao/dao/ITransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techment.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.techment.entity.Transaction;
-
-public interface ITransactionRepository extends JpaRepository<Transaction, Long>{
-
-}
