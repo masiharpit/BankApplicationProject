29f11cbb5d2d145a52a9afab7a5455eb33eb7074
katyayanisahu <88482844+katyayanisahu@users.noreply.github.com>
2021-10-08 19:30:52 +0700

    Add files via upload

---
+++ BankingApplication/src/main/java/com/techment/services/CustomerServiceImpl.java
@@ -0,0 +1,104 @@
+package com.techment.service;
+
+import java.util.List;
+
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.techment.Dao.ICustomerDao;
+import com.techment.dto.CustomerDto;
+import com.techment.entity.Customer;
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	@Autowired
+	private ICustomerDao Dao;
+	
+//    @Override
+//	public Customer addCustomer(Customer customer) {
+//		Customer customer1 = new Customer();
+//		//CustomerDto customerDto;
+//		if(customer.getCustomerId()!=0)
+//		{
+//			customer1.setCustomerId(customer.getCustomerId());
+//		}
+//		customer1.setCustomerName(customer.getCustomerName());
+//		//customer.setAddress(customer.getAddress());
+//		customer1.setAge(customer.getAge());
+//		customer1.setEmailID(customer.getEmailID());
+//		customer1.setPhoneNo(customer.getPhoneNo());
+//		return Dao.save(customer1);
+//	}
+	
+	
+	public Customer addCustomer(CustomerDto customerdto) {
+		CustomerDto.save(customerdto);
+	   return "Customer Details Added Sucessfully";
+}
+
+	
+
+	@Override
+	public Customer updateCustomer(long CustomerId,CustomerDto customerdto) {
+		//Integer customerId = null;
+		Customer existingcustomer = Dao.findCustomerById(CustomerId);
+		existingcustomer.setCustomerName(customerdto.getCustomerName());
+		existingcustomer.setPhoneNo(customerdto.getPhoneNo());
+		existingcustomer.setEmailID(customerdto.getEmailID());		
+		//existingcustomer.setAccount(customerdto.getAccount());		
+		existingcustomer.setAge(customerdto.getAge());
+			
+		Dao.save(existingcustomer);
+		return existingcustomer;
+		
+}
+
+	@Override
+	public boolean deleteCustomer(long customer_id) {
+		Dao.deleteById(customer_id);
+		return true;
+		
+	}
+
+	@Override
+	public List<CustomerDto> listAllCustomer(double minBalance) {
+
+		
+		//return Dao.findAll();
+	}
+   
+	
+	
+    @Override
+	public CustomerDto viewCustomerDetails(long customer_id) {
+	
+	Customer customer =  Dao.findById(customer_id).get();
+	CustomerDto customerDto= new CustomerDto(customer.getCustomerId(),customer.getCustomerName(),customer.getPhoneNo(),customer.getEmailID(),customer.getAge());
+
+	//return customer;
+		
+		return (CustomerDto) Dao.findAll();		
+	}
+
+	@Override
+	public CustomerDto findCustomerById(long customer_id) {
+		
+		Customer customer =  Dao.findById(customer_id).get();
+		CustomerDto customerDto= new CustomerDto(customer.getCustomerId(),customer.getCustomerName(),customer.getPhoneNo(),customer.getEmailID(),customer.getAge());
+
+		//return customer;
+			
+			return (CustomerDto) Dao.findAll();		
+
+	}
+}	
+	
+//	@Override
+//	public List<Customer> getAllCustomers() {
+//				return Dao.findAll();
+//		
+//   
+//}
