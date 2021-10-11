ab8a43e022e83e5f99a78e546875321ab9b1fc58
Divya Kashyap <56669225+kashyapdivu@users.noreply.github.com>
2021-10-11 16:57:05 +0530

    Add files via upload

--- BankingApplication/src/main/java/com/techment/dao/AdminRepository.java
+++ BankingApplication/src/main/java/com/techment/dao/AdminRepository.java
@@ -3,12 +3,8 @@ package com.techment.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.techment.Entity.Admin;
-import com.techment.dto.Admindto;
+import com.techment.entity.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Long>{
-
-	void save(Admindto admin);
-
 
 }
