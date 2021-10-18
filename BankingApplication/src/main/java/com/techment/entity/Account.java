f3796a2f4a932e76b83e4002fc3687d5af501f35
gitikapinjani1 <88777281+gitikapinjani1@users.noreply.github.com>
2021-10-18 13:41:13 +0530

    Add files via upload

--- BankingApplication/src/main/java/com/techment/entity/Account.java
+++ BankingApplication/src/main/java/com/techment/entity/Account.java
@@ -6,33 +6,64 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import com.techment.enumClass.AccountType;
+
 
 @Entity
-@Table(name="account5")
+@Table(name="account5000")
 public class Account {
 	
 	@Id
-	long accountId;
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long accountId;
 	
 	@Column(name="interest_Rate")
-	double interestRate;
+	private double interestRate;
 	
 	@Column(name="balance")
-	double balance;
-	
+	private double balance;
+
 	@Column(name="date_Of_Opening")
-	LocalDate dateOfOpening;
+	private LocalDate dateOfOpening;
 	
+	private AccountType accountType;
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="customerId", referencedColumnName = "customerId")
+	private Customer customer;
+		
 //@OneToMany(mappedBy = "nomineeId",cascade=CascadeType.ALL)
 //	@JoinColumn(name = "nomineeId")
-//	List<Nominee> nominee; 
+//	List<Nominee> nominee;
 	
-	@OneToMany(mappedBy = "beneficiaryId",cascade=CascadeType.ALL)
-	List<Beneficiary> beneficiary; 
+	
+	
+	@Enumerated(EnumType.ORDINAL)
+	public AccountType getAccountType() {
+		return accountType;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public void setAccountType(AccountType accountType) {
+		this.accountType = accountType;
+	}
+
 	
 	public long getAccountId() {
 		return accountId;
