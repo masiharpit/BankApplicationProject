52fcc456a758cbbec5c8b700bd49601bbbf80b66
gitikapinjani1 <88777281+gitikapinjani1@users.noreply.github.com>
2021-10-18 13:43:37 +0530

    Add files via upload

--- BankingApplication/src/main/java/com/techment/service/IAccountService.java
+++ BankingApplication/src/main/java/com/techment/service/IAccountService.java
@@ -1,15 +1,18 @@
 package com.techment.service;
 
-import java.util.List;
 
 import com.techment.dto.AccountDto;
+import com.techment.dto.CustomerDto;
+import com.techment.dto.TransactionDto;
 import com.techment.entity.Account;
 
 public interface IAccountService {
-	AccountDto transferMoney(long senderAccountId, long receiverAccountId, double amount , String username , String password );
-	AccountDto withdraw(long AccountId, String username, String password, double amount );
-	AccountDto deposite(long accountId, double amount) ;
 	
- 
-
+	TransactionDto transferMoney(long senderAccountId, long receiverAccountId, double amount , String username , String password );
+	TransactionDto withdraw(long AccountId, String username, String password, double amount );
+	TransactionDto deposit(long accountId, double amount) ;
+	AccountDto findAccountById(long accountId);
+	AccountDto addAccount(AccountDto accountDto, long CustomerId);
+	AccountDto updateAccount(AccountDto accountDto);
+	AccountDto deleteAccount(long accountId);
 }
