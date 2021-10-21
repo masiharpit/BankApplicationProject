8f7c52df405d1c91b58bce0751b9f21a13c5b806
katyayanisahu <88482844+katyayanisahu@users.noreply.github.com>
2021-10-21 12:15:37 +0700

    Add files via upload

---
+++ BankingApplication/src/main/java/com/techment/service/ICustomerService.java
@@ -0,0 +1,18 @@
+package com.techment.service;
+import java.util.List;
+import java.util.Set;
+
+import com.techment.dto.CustomerDto;
+import com.techment.entity.Customer;
+public interface ICustomerService {
+
+	
+	CustomerDto addCustomer(CustomerDto customer);
+	CustomerDto  updateCustomer(long CustomerId,CustomerDto customer); 
+    boolean  deleteCustomer(long customer_id); 
+	//List<CustomerDto> listAllCustomer(double minBalance);
+	List<CustomerDto> viewCustomerDetails();
+	CustomerDto findCustomerById(long customer_id);
+	CustomerDto LoginUser(long customer_id, String customer_name);
+	
+}
